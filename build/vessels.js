e252db00a8ca3cbc7054201e725c5e83afc417e8
humanapp <>
2023-09-13 13:34:00 -0700

    build

--- build/vessels.js
+++ build/vessels.js
@@ -94,10 +94,13 @@ async function refreshVesselStatusAsync() {
         const hasOutOfServiceAlert = (name) => {
             name = name.toLowerCase();
             for (let bull of bulls) {
-                const text = bull.BulletinText.toLowerCase();
-                if (/out of service/.test(text)) {
-                    if (text.includes(name))
-                        return true;
+                const lower = bull.BulletinText.toLowerCase();
+                const texts = lower.split("<p>");
+                for (const text of texts) {
+                    if (/out of service/.test(text)) {
+                        if (text.includes(name))
+                            return true;
+                    }
                 }
             }
             return false;
